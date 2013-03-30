2936c6af5182ebaad0760c9bf0d0328960bcde37
Ryan Yogan <ryanyogan@gmail.com>
2013-03-29 22:39:20 -0500

    Basic seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# First post since there is no web gui for creating a post
+
+Post.create! title: "Get your learn on",
+             author: "Ryan Yogan",
+             intro: "Where do I go after I watched Tom Dale's latest screencast on getting started with Ember.js?
+             Everyone at HackerNews yelled at me when I asked a question, I'm kidding!",
+             extended: "There are some great resources and I am going to turn this project into a home
+             to find those places, as well as turning this project into a place to come back weekly and
+             continue working incremently on new features building off the basics.  For now please see 
+             the about page and checkout the basic code in Github on how to serve a Ember app via Rails."
+
+Post.create! title: "Go ahead and..",
+             author: "Anyone",
+             intro: "The other post is locked so you can edit this one as much as you'd like.",
+             extended: "Yes as you can guess some of the next features to add will be a full crud interface with
+             user authentication"
